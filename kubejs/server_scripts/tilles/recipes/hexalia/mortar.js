08800254660a95b438e432635119b2f4b1a5639e
tilles111 <59595469+tilles111@users.noreply.github.com>
2026-05-15 18:22:18 -0700

    Mortar batch crafting
    Grind my crystals

--- kubejs/server_scripts/tilles/recipes/hexalia/mortar.js
+++ kubejs/server_scripts/tilles/recipes/hexalia/mortar.js
@@ -11,6 +11,39 @@ ServerEvents.recipes(event => {
             item: "minecraft:gravel"
         }
     })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "minecraft:cobblestone"
+            },
+            {
+                item: "minecraft:cobblestone"
+            }
+        ],
+        output: {
+            item: "minecraft:gravel",
+            count: 2
+        }
+    })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "minecraft:cobblestone"
+            },
+            {
+                item: "minecraft:cobblestone"
+            },
+            {
+                item: "minecraft:cobblestone"
+            }
+        ],
+        output: {
+            item: "minecraft:gravel",
+            count: 3
+        }
+    })
     //Also maybe a placeholder?
     event.custom({
         type: "hexalia:mortar_and_pestle",
@@ -23,6 +56,39 @@ ServerEvents.recipes(event => {
             item: "minecraft:sand"
         }
     })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "minecraft:gravel"
+            },
+            {
+                item: "minecraft:gravel"
+            }
+        ],
+        output: {
+            item: "minecraft:sand",
+            count: 2
+        }
+    })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "minecraft:gravel"
+            },
+            {
+                item: "minecraft:gravel"
+            },
+            {
+                item: "minecraft:gravel"
+            }
+        ],
+        output: {
+            item: "minecraft:sand",
+            count: 3
+        }
+    })
     
     event.custom({
         type: "hexalia:mortar_and_pestle",
@@ -33,6 +99,39 @@ ServerEvents.recipes(event => {
         ],
         output: {
             item: "forbidden_arcanus:arcane_crystal_dust"
+        }
+    })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "forbidden_arcanus:arcane_crystal"
+            },
+            {
+                item: "forbidden_arcanus:arcane_crystal"
+            }
+        ],
+        output: {
+            item: "forbidden_arcanus:arcane_crystal_dust",
+            count: 2
+        }
+    })
+    event.custom({
+        type: "hexalia:mortar_and_pestle",
+        ingredients: [
+            {
+                item: "forbidden_arcanus:arcane_crystal"
+            },
+            {
+                item: "forbidden_arcanus:arcane_crystal"
+            },
+            {
+                item: "forbidden_arcanus:arcane_crystal"
+            }
+        ],
+        output: {
+            item: "forbidden_arcanus:arcane_crystal_dust",
+            count: 3
         }
     })
 
--- kubejs/server_scripts/tilles/tags/item.js
+++ kubejs/server_scripts/tilles/tags/item.js
@@ -1,3 +1,4 @@
 ServerEvents.tags('item', event => {
+  event.add('caupona:wolfberries', 'ars_nouveau:sourceberry_bush')
   event.add('caupona:sourceberries', 'ars_nouveau:sourceberry_bush')
 })
