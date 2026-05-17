5011360b039427a8920db25656dbf7b110d6393c
MelodyCube <1152a0xy425@gmail.com>
2026-05-17 08:08:01 -0400

    ok sorry tilles youre right the main is weird and keeps remaking itself
    but id rather it be hidden

--- kubejs/server_scripts/recipes/minecraft/lead_iron_compat.js
+++ kubejs/server_scripts/recipes/minecraft/lead_iron_compat.js
@@ -1,7 +1,27 @@
 ServerEvents.recipes(event => {
     const ironLeadItems = [
         '#supplementaries:candle_holders',
-        'decorative_blocks:chain', 'minecraft:chain', 'supplementaries:sconce_soul', 'supplementaries:sconce', 'minecraft:soul_lantern', 'minecraft:lantern', 'supplementaries:goblet', 'architects_palette:tread_plate', 'architects_palette:plating_block', 'embers:tinker_hammer', Item.of('farmersdelight:skillet', '{Damage:0}'), 'farmersdelight:cooking_pot', 'caupona:situla', 'minecraft:hopper', 'supplementaries:cage', 'minecraft:cauldron', 'minecraft:anvil', 'sawmill:sawmill', 'minecraft:smithing_table', 'minecraft:stonecutter', 'quark:framed_glass'
+        'decorative_blocks:chain',
+        'minecraft:chain',
+        'supplementaries:sconce_soul',
+        'supplementaries:sconce',
+        'minecraft:soul_lantern',
+        'minecraft:lantern',
+        'supplementaries:goblet',
+        'architects_palette:tread_plate',
+        'architects_palette:plating_block',
+        'embers:tinker_hammer',
+        'farmersdelight:skillet',
+        'farmersdelight:cooking_pot',
+        'caupona:situla',
+        'minecraft:hopper',
+        'supplementaries:cage',
+        'minecraft:cauldron',
+        'minecraft:anvil',
+        'sawmill:sawmill',
+        'minecraft:smithing_table',
+        'minecraft:stonecutter',
+        'quark:framed_glass'
     ]
     event.replaceInput(
         { output: ironLeadItems },
