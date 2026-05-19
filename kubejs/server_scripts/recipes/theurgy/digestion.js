13541501d541ea7a911ccd5eefac5610ad805c6c
MelodyCube <1152a0xy425@gmail.com>
2026-05-19 19:05:04 -0400

    more cleaning and helper functions and such

--- kubejs/server_scripts/recipes/theurgy/digestion.js
+++ kubejs/server_scripts/recipes/theurgy/digestion.js
@@ -1,104 +1,87 @@
 ServerEvents.recipes(event => {
-    event.remove({id: '/theurgy:.*digestion.*/'})
-    event.custom({
-        type: 'theurgy:digestion',
-        fluid: {
-            fluid: "kubejs:sourceberry_stew"
-        },
-        fluidAmount: 750,
-        ingredients: [
-            {
-                count: 4,
-                item: 'theurgy:alchemical_sulfur_gravel'
-            },
-            {
-                count: 1,
-                item: 'kubejs:aqueous_salt'
-            }
-        ],
-        result: { 
-            Count: 2,
-            id: 'theurgy:alchemical_sulfur_lead'
-        },
-        time: 600
-    })
 
-    event.custom({
-        type: 'theurgy:digestion',
-        fluid: {
-            fluid: "kubejs:sourceberry_stew"
-        },
-        fluidAmount: 250,
-        ingredients: [
-            {
-                count: 1,
-                item: 'theurgy:alchemical_sulfur_lead'
+    event.remove({id: '/theurgy:.*digestion.*/'})
+
+    function digestion(output, input_items, input_fluid, time, id) {
+
+        if (time == undefined) {
+            time = 600
+        }
+        if (id == undefined) {
+            let id_part = input_items.map(input_item => Item.of(input_item).id.split(':')[1])
+            id = `kubejs:digestion/${id_part.join('_and_')}_in_${Fluid.of(input_fluid).id.split(':')[1]}_to_${Item.of(output).id.split(':')[1]}`
+        }
+
+        event.custom({
+            type: 'theurgy:digestion',
+            fluid: {
+                fluid: Fluid.of(input_fluid).id
             },
-            {
-                count: 1,
-                item: 'kubejs:infernal_salt'
+            fluidAmount: Fluid.of(input_fluid).amount,
+            ingredients: input_items.map(input_item => Item.of(input_item).toJson()),
+            result: {
+                Count: Item.of(output).count,
+                id: Item.of(output).id
             },
-            {
-                count: 1,
-                item: 'kubejs:aqueous_salt'
-            }
+            time: time
+        }).id(id)
+
+    }
+
+    digestion(Item.of('theurgy:alchemical_sulfur_lead', 2),
+        [
+            Item.of('theurgy:alchemical_sulfur_gravel', 4),
+            'kubejs:aqueous_salt'
         ],
-        result: { 
-            Count: 2,
-            id: 'theurgy:alchemical_sulfur_tin'
-        },
-        time: 600
-    })
-    event.custom({
-        type: 'theurgy:digestion',
-        fluid: {
-            fluid: "kubejs:sourceberry_stew"
-        },
-        fluidAmount: 250,
-        ingredients: [
-            {
-                count: 1,
-                item: 'theurgy:alchemical_sulfur_tin'
-            },
-            {
-                count: 1,
-                item: 'kubejs:earthen_salt'
-            },
-            {
-                count: 1,
-                item: 'kubejs:aqueous_salt'
-            }
+        Fluid.of('kubejs:sourceberry_stew', 750)
+    )
+
+    // event.custom({
+    //     type: 'theurgy:digestion',
+    //     fluid: {
+    //         fluid: "kubejs:sourceberry_stew"
+    //     },
+    //     fluidAmount: 750,
+    //     ingredients: [
+    //         {
+    //             count: 4,
+    //             item: 'theurgy:alchemical_sulfur_gravel'
+    //         },
+    //         {
+    //             count: 1,
+    //             item: 'kubejs:aqueous_salt'
+    //         }
+    //     ],
+    //     result: { 
+    //         Count: 2,
+    //         id: 'theurgy:alchemical_sulfur_lead'
+    //     },
+    //     time: 600
+    // })
+
+    digestion(Item.of('theurgy:alchemical_sulfur_tin', 2),
+        [
+            'theurgy:alchemical_sulfur_lead',
+            'kubejs:infernal_salt',
+            'kubejs:aqueous_salt'
         ],
-        result: { 
-            Count: 2,
-            id: 'theurgy:alchemical_sulfur_iron'
-        },
-        time: 600
-    })
-    event.custom({
-        type: 'theurgy:digestion',
-        fluid: {
-            fluid: "kubejs:sourceberry_stew"
-        },
-        fluidAmount: 250,
-        ingredients: [
-            {
-                count: 1,
-                item: 'theurgy:alchemical_sulfur_iron'
-            },
-            {
-                count: 1,
-                item: 'kubejs:aerial_salt'
-            },
-            {
-                count: 1,
-                item: 'kubejs:aqueous_salt'
-            }
+        Fluid.of('kubejs:sourceberry_stew', 250)
+    )
+    digestion(Item.of('theurgy:alchemical_sulfur_iron', 2),
+        [
+            'theurgy:alchemical_sulfur_tin',
+            'kubejs:earthen_salt',
+            'kubejs:aqueous_salt'
         ],
-        result: { 
-            Count: 2,
-            id: 'theurgy:alchemical_sulfur_copper'
-        },
-        time: 600
-    })
+        Fluid.of('kubejs:sourceberry_stew', 250)
+    )
+    digestion(Item.of('theurgy:alchemical_sulfur_copper', 2),
+        [
+            'theurgy:alchemical_sulfur_iron',
+            'kubejs:aerial_salt',
+            'kubejs:aqueous_salt'
+        ],
+        Fluid.of('kubejs:sourceberry_stew', 250)
+    )
+    
 })
