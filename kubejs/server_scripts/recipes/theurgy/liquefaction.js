95b1c0cb1de255f27d745b131560314e695ed55d
tilles111 <59595469+tilles111@users.noreply.github.com>
2026-05-28 16:31:52 -0700

    DO NOT NUKE LIQUEFACTION IT BREAKS EVERYTHING

--- kubejs/server_scripts/recipes/theurgy/liquefaction.js
+++ kubejs/server_scripts/recipes/theurgy/liquefaction.js
@@ -1,4 +1,4 @@
 ServerEvents.recipes(event => {
-    event.remove({id: '/theurgy:.*liquefaction.*/'})
+    // event.remove({id: '/theurgy:.*liquefaction.*/'})
     // need liquefaction recipe for rendering and EMI to work, weird and bad!!!
 })
